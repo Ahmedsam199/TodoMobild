e679510737142fe62426f57317bb0d5483280e1a
Ahmedsam199 <98406575+Ahmedsam199@users.noreply.github.com>
2023-05-04 13:56:06 +0300

    first commit

---
+++ app/src/main/java/com/example/notbasictodolist/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.notbasictodolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+Button signButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+
+        signButton=findViewById(R.id.SignButton);
+        signButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,MainPage.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
