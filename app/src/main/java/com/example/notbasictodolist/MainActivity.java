2a347ca580979ce189e9d77c4239624528a4b296
Ahmedsam199 <98406575+Ahmedsam199@users.noreply.github.com>
2023-05-10 23:51:58 +0300

    NFC QR CODE

--- app/src/main/java/com/example/notbasictodolist/MainActivity.java
+++ app/src/main/java/com/example/notbasictodolist/MainActivity.java
@@ -1,24 +1,77 @@
 package com.example.notbasictodolist;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
 import android.app.Dialog;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.nfc.FormatException;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-Button signButton;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity implements NfcAdapter.ReaderCallback {
+Button signButton,SignWithQR;
+
+    private PendingIntent pendingIntent;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_CAMERA_PERMISSION = 1;
+    private NfcAdapter nfcAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
-        setContentView(R.layout.activity_main);
 
+        setContentView(R.layout.activity_main);
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        SignWithQR=findViewById(R.id.SignQRCode);
         signButton=findViewById(R.id.SignButton);
-        signButton.setOnClickListener(new View.OnClickListener() {
+        SignWithQR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+                } else {
+
+                    // we need to create the object
+                    // of IntentIntegrator class
+                    // which is the class of QR library
+                    IntentIntegrator intentIntegrator = new IntentIntegrator(MainActivity.this);
+                    intentIntegrator.setPrompt("Scan a barcode or QR Code");
+                    intentIntegrator.setOrientationLocked(true);
+                    intentIntegrator.initiateScan();
+
+                }
+            }
+        });
+
+        // Create an intent to launch this activity when an NFC tag is detected
+        Intent intent = new Intent(this, getClass());
+
+    signButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -27,4 +80,96 @@ Button signButton;
             }
         });
     }
-}
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        // if the intentResult is null then
+        // toast a message as "cancelled"
+        if (intentResult != null) {
+            if (intentResult.getContents() == null) {
+                Toast.makeText(getBaseContext(), "Cancelled", Toast.LENGTH_SHORT).show();
+            } else {
+                // if the intentResult is not null we'll set
+                // the content and format of scan message
+                System.out.println("First "+intentResult.getContents().toString());
+                Intent intent=new Intent(MainActivity.this,MainPage.class);
+                startActivity(intent);
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (nfcAdapter != null) {
+            nfcAdapter.enableReaderMode(this, this, NfcAdapter.FLAG_READER_NFC_A, null);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (nfcAdapter != null) {
+            nfcAdapter.disableReaderMode(this);
+        }
+    }
+
+    @Override
+    public void onTagDiscovered(Tag tag) {
+        // Handle tag discovery here
+        System.out.println(tag);
+        if (tag == null) {
+            return;
+        }
+
+        // Get the tag ID (in bytes)
+        byte[] id = tag.getId();
+
+
+        // Get the NDEF message from the tag (if available)
+        Ndef ndef = Ndef.get(tag);
+        if (ndef != null) {
+            try {
+                ndef.connect();
+                NdefMessage message = ndef.getNdefMessage();
+                if (message != null) {
+                    Log.d(TAG, "NDEF message: " + message.toString());
+                    NdefRecord[] records = message.getRecords();
+                    for (NdefRecord record : records) {
+                        if (record.getTnf() == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(record.getType(), NdefRecord.RTD_TEXT)) {
+                            byte[] payload = record.getPayload();
+                            String textEncoding = ((payload[0] & 0x80) == 0) ? "UTF-8" : "UTF-16";
+                            int languageCodeLength = payload[0] & 0x3F;
+                            String text = new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
+                            Log.d("MainActivity", "Text content: " + text);
+                            Intent intent=new Intent(MainActivity.this,MainPage.class);
+                            startActivity(intent);
+                            break;
+                        }
+                    }
+                } else {
+                    Log.d(TAG, "No NDEF message found on tag");
+                }
+            } catch (IOException | FormatException e) {
+                Log.e(TAG, "Error reading NDEF message", e);
+            } finally {
+                try {
+                    ndef.close();
+                } catch (IOException e) {
+                    Log.e(TAG, "Error closing NDEF connection", e);
+                }
+            }
+        } else {
+            Log.d(TAG, "Tag doesn't support NDEF");
+        }
+    }
+
+    private String bytesToHexString(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%02x", b & 0xff));
+        }
+        return sb.toString();}}
+
